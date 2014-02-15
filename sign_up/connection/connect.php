1f0aeef801a398a25c2e5fcdaf219692cd7f6550
roowako <roowbergonia@gmail.com>
2014-02-15 09:33:33 -0800

    a

--- sign_up/connection/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    DEFINE("DB_HOST","localhost");
-    DEFINE("DB_USERNAME","root");
-    DEFINE("DB_PASSWORD","");
-    DEFINE("DB_NAME","source");
-
-    $con = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
-?>
