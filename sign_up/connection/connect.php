cc294eb2993c870eb680757c77cc23b7cd8404cb
roowako <roowbergonia@gmail.com>
2014-02-15 09:03:42 -0800

    Basic registration

---
+++ sign_up/connection/connect.php
@@ -0,0 +1,9 @@
+<?php
+    
+    DEFINE("DB_HOST","localhost");
+    DEFINE("DB_USERNAME","root");
+    DEFINE("DB_PASSWORD","");
+    DEFINE("DB_NAME","source");
+
+    $con = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+?>
